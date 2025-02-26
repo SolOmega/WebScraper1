267cc0358c8276ba404b4102ed442bcb877958eb
SolOmega <jequanj28@gmail.com>
2025-02-25 21:44:49 -0600

    virtual environment main gitignore and requirements

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-# This is a test
+import requests
+from bs4 import BeautifulSoup
+
+
+# Makes GET request
+r = requests.get("https://www.twitch.tv/")
+
+# Checks status code and success code
+
+soup = BeautifulSoup(r.content, 'html.parser')
+print(soup.prettify())
